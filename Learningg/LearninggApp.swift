b5a07c72e5090b3612b8c846ffd6954bde0b259e
alasirimaya <alasirimaya@gmail.com>
2024-11-04 14:44:20 +0300

    Initial Commit

---
+++ Learningg/LearninggApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearninggApp.swift
+//  Learningg
+//
+//  Created by maya alasiri  on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearninggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
